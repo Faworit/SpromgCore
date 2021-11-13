92ebc5c3b6d8a2ac2b5446694dcbdb3ac822ff21
Sergey_Ryabtsev <Sergey_Ryabtsev@epam.com>
2021-11-13 16:55:32 +0600

    1. add test

--- src/main/java/com/epam/ryabtsev/DAO/storage/Storage.java
+++ src/main/java/com/epam/ryabtsev/DAO/storage/Storage.java
@@ -3,7 +3,6 @@ package com.epam.ryabtsev.DAO.storage;
 import com.epam.ryabtsev.model.Event;
 import com.epam.ryabtsev.model.Ticket;
 import com.epam.ryabtsev.model.User;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -11,18 +10,25 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-@Scope("singleton")
 public class Storage {
 
     private Map<Long, User> users;
     private Map<Long, Event> events;
     private Map<Long, Ticket> tickets;
+    private static Storage storage = null;
 
-    @PostConstruct
-    public void init() {
+    private Storage() {
         this.users = new ConcurrentHashMap<>();
         this.events = new ConcurrentHashMap<>();
         this.tickets = new ConcurrentHashMap<>();
+    }
+
+    @PostConstruct
+    public static Storage init() {
+        if (storage == null) {
+            storage = new Storage();
+        }
+        return storage;
     }
 
     public Map<Long, User> getUsers() {
